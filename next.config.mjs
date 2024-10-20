8d2fb7be72301280ef0a8296e1c8016e46f9d289
Israel <bamideleisrael04@gmail.com>
2024-10-20 19:47:17 +0100

    fix: update images configuration to use remotePatterns
    - Replaced the deprecated 'images.domains' configuration with 'images.remotePatterns' to allow images from external sources.
    - Added remote pattern definitions for the following domains:
      - 'https://avatars.githubusercontent.com': Allows loading user avatars from GitHub.
      - 'https://nextjs.org': Enables image loading from Next.js documentation site.
    - This change ensures compatibility with the latest Next.js versions and addresses warnings about deprecated configuration options.
    
    This update enhances the image handling capabilities of the application, adhering to the recommended practices in Next.js for remote images.

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,20 @@
 const nextConfig = {
     reactStrictMode: true, // Enables React's Strict Mode for identifying potential problems
     images: {
-        domains: ['avatars.githubusercontent.com', 'nextjs.org'], // Allow images from these specified domains
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'avatars.githubusercontent.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'nextjs.org',
+                port: '',
+                pathname: '/**',
+            },
+        ],
     },
 };
 
